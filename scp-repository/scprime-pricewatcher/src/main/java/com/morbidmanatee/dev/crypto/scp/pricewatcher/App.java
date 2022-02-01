44aced9d556e44a4246c3f1b2c69f45758e9c857
morbidmanatee <morbidmanatee2022@gmail.com>
2022-02-01 14:36:37 -0500

    Cleanup

--- scp-repository/scprime-pricewatcher/src/main/java/com/morbidmanatee/dev/crypto/scp/pricewatcher/App.java
+++ scp-repository/scprime-pricewatcher/src/main/java/com/morbidmanatee/dev/crypto/scp/pricewatcher/App.java
@@ -56,7 +56,7 @@ public class App
     	{
     		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
     		Date date = new Date(System.currentTimeMillis());
-    		System.out.println("# " + formatter.format(date));   		
+    		System.out.println("# Generated " + formatter.format(date));   		
     	}
     	System.out.println("#");
     	
